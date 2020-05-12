45425b5bc23ad5190f7e2fb59b290a2a8bd9d331
vinothvenugopal <61199725+vinothvenugopal@users.noreply.github.com>
2020-05-12 22:21:17 +0530

    Day1_programs upload

---
+++ src/main/java/javaWorkout/Day1_Occourance_Logic2.java
@@ -0,0 +1,29 @@
+package javaWorkout;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Day1_Occourance_Logic2 {
+
+	public static void main(String[] args) {
+
+		String input = "You have no choice other than following me!";
+		char[] inputArray = input.toCharArray();
+		Map<Character, Integer> count = new HashMap<Character, Integer>();
+		for (int i = 0; i < inputArray.length; i++) 
+		{
+			if(count.containsKey(inputArray[i]))
+			{
+				count.put(inputArray[i], count.get(inputArray[i])+1);
+			}
+			else
+			{
+				count.put(inputArray[i], 1);
+			}
+		}
+
+		System.out.println("Occurance of o in the given input String is "+count.get('Y'));
+		}
+	}
+
+
